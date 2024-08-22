4d5d10046797d36a1f6aad64065bd36a8814697e
GRX005 <fodisoma@gmail.com>
2024-08-22 21:28:26 +0200

    Add project files.

---
+++ Tutorial/main.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+mt19937_64 gen(random_device{}());
+uniform_int_distribution<> intDistr(1, 1000);
+void genAAn(vector<unsigned long long>& local_v);
+unsigned long long y;
+unsigned long long z;
+
+int main() {
+#if 0
+	int a[5];
+	a[0] = 1;
+	a[1] = 23;
+	cout << a[0] + a[1] << endl;
+	setlocale(LC_ALL, "hun");
+	string őű = "asdáéőúűöüó";
+	cout << őű;
+#endif // 0
+	setlocale(LC_ALL, "hun");
+#if 0
+	int a[5];
+	for (int i = 0; i < 5; i++) {
+		cout << "Add meg az " << i + 1 << ". számot!" << endl;
+		cin >> a[i];
+	}
+	cout << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << i + 1 << ". elem: " << a[i] << endl;
+	}
+#endif // 0
+	vector<thread> threads;
+	vector<unsigned long long> v;
+
+	cout << "Hany szamot akarsz megadni?" << endl;
+	cin >> y;
+	auto start = chrono::steady_clock::now();
+	z = y/2;
+	unsigned long long x = y / z;
+	vector<vector<unsigned long long>> results(x);
+	for (unsigned long long i = 0; i < x; i++) {
+		threads.push_back(thread(genAAn, ref(results[i])));
+		cout << "new\n";
+	}
+	for (thread &t : threads) {
+		t.join();
+	}
+	for (const auto& local_v : results) {
+		v.insert(v.end(), local_v.begin(), local_v.end());
+	}
+	if (y % 2 != 0) {
+		v.pop_back();
+	}
+	auto end = chrono::steady_clock::now();
+	cout << "\nTime passed: " << chrono::duration_cast<chrono::milliseconds>(end - start) << endl;
+	cout << "Vector size: " << v.size() << endl;
+	for (ptrdiff_t i = 0; i < v.size(); i++) {
+		cout << "Number " << i + 1 << ": " << v[i] << endl;
+	}
+	return 0;
+} 
+
+void genAAn(vector<unsigned long long>& local_v) {
+	for (ptrdiff_t i = 0; i < z; i++)
+	{
+		//lock_guard<mutex> lock(mtx);
+		local_v.push_back(intDistr(gen));
+	}
+	if (y % 2 != 0) {
+		local_v.push_back(intDistr(gen));
+	}
+}
