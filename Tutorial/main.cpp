52cfa94b9d832afc22a807ec2d52f6ed6e23d5bb
GRX005 <fodisoma@gmail.com>
2024-08-22 21:44:00 +0200

    asd

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -22,7 +22,6 @@ int main() {
 	string őű = "asdáéőúűöüó";
 	cout << őű;
 #endif // 0
-	setlocale(LC_ALL, "hun");
 #if 0
 	int a[5];
 	for (int i = 0; i < 5; i++) {
@@ -37,7 +36,7 @@ int main() {
 	vector<thread> threads;
 	vector<unsigned long long> v;
 
-	cout << "Hany szamot akarsz megadni?" << endl;
+	cout << "How many numbers do you want to generate?" << endl;
 	cin >> y;
 	auto start = chrono::steady_clock::now();
 	z = y/2;
@@ -45,33 +44,30 @@ int main() {
 	vector<vector<unsigned long long>> results(x);
 	for (unsigned long long i = 0; i < x; i++) {
 		threads.push_back(thread(genAAn, ref(results[i])));
-		cout << "new\n";
+		cout << "new thread\n";
 	}
 	for (thread &t : threads) {
 		t.join();
 	}
+	if (y % 2 != 0) {
+		results[0].push_back(intDistr(gen));
+	}
 	for (const auto& local_v : results) {
 		v.insert(v.end(), local_v.begin(), local_v.end());
-	}
-	if (y % 2 != 0) {
-		v.pop_back();
 	}
 	auto end = chrono::steady_clock::now();
 	cout << "\nTime passed: " << chrono::duration_cast<chrono::milliseconds>(end - start) << endl;
 	cout << "Vector size: " << v.size() << endl;
-	for (ptrdiff_t i = 0; i < v.size(); i++) {
+	for (size_t i = 0; i < v.size(); i++) {
 		cout << "Number " << i + 1 << ": " << v[i] << endl;
 	}
 	return 0;
 } 
 
 void genAAn(vector<unsigned long long>& local_v) {
-	for (ptrdiff_t i = 0; i < z; i++)
+	for (size_t i = 0; i < z; i++)
 	{
 		//lock_guard<mutex> lock(mtx);
-		local_v.push_back(intDistr(gen));
-	}
-	if (y % 2 != 0) {
 		local_v.push_back(intDistr(gen));
 	}
 }
