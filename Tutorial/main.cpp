62a23d639e065ce500afcef9e7e06f4957f08770
GRX005 <fodisoma@gmail.com>
2024-08-23 01:48:27 +0200

    Commit2

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 #include <random>
 #include <thread>
-#include <mutex>
 #include <chrono>
 
 using namespace std;
 mt19937_64 gen(random_device{}());
-uniform_int_distribution<> intDistr(1, 1000);
+uniform_int_distribution<> intDistr(1, 100000);
 void genAAn(vector<unsigned long long>& local_v);
+void CLI();
+bool bCLI = true;
 unsigned long long y;
 unsigned long long z;
 
@@ -38,14 +39,17 @@ int main() {
 
 	cout << "How many numbers do you want to generate?" << endl;
 	cin >> y;
+	cout << "In what range? (Ex: 1-10000)" << endl;
+
 	auto start = chrono::steady_clock::now();
 	z = y/2;
 	unsigned long long x = y / z;
 	vector<vector<unsigned long long>> results(x);
 	for (unsigned long long i = 0; i < x; i++) {
 		threads.push_back(thread(genAAn, ref(results[i])));
-		cout << "new thread\n";
+		cout << "New generator thread initialized.\n";
 	}
+	thread cliThread(CLI);
 	for (thread &t : threads) {
 		t.join();
 	}
@@ -55,11 +59,18 @@ int main() {
 	for (const auto& local_v : results) {
 		v.insert(v.end(), local_v.begin(), local_v.end());
 	}
+	bCLI = false;
+	cliThread.join();
 	auto end = chrono::steady_clock::now();
-	cout << "\nTime passed: " << chrono::duration_cast<chrono::milliseconds>(end - start) << endl;
+	cout << "Time passed: " << chrono::duration_cast<chrono::milliseconds>(end - start) << endl;
 	cout << "Vector size: " << v.size() << endl;
-	for (size_t i = 0; i < v.size(); i++) {
-		cout << "Number " << i + 1 << ": " << v[i] << endl;
+	cout << "Do you want to see all generated numbers? [Y/N] ";
+	char c;
+	cin >> c;
+	if (c == 'Y') {
+		for (size_t i = 0; i < v.size(); i++) {
+			cout << "Number " << i + 1 << ": " << v[i] << endl;
+		}
 	}
 	return 0;
 } 
@@ -70,4 +81,35 @@ void genAAn(vector<unsigned long long>& local_v) {
 		//lock_guard<mutex> lock(mtx);
 		local_v.push_back(intDistr(gen));
 	}
+}
+
+void CLI() {
+	const string b = "Generating";
+	const string c = ".";
+	const string a = string(13, ' ') + "\r";
+	int i = 0;
+	while (bCLI)
+	{
+		switch (i)
+		{
+		case 0:
+			cout << a;
+			cout << b + c;
+			i++;
+			break;
+		case 1:
+			cout << b + c + c;
+			i++;
+			break;
+		case 2:
+			cout << b+c+c+c;
+			i = 0;
+			break;
+		default:
+			break;
+		}
+		cout << "\r";
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
+	cout << a;
 }
